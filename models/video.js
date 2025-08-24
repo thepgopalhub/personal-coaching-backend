cbc908cc1071cf1b17fb8b8f271cfac3e2b3de01
Gopal Choudhary <gopalchoughary2486@gmail.com>
2025-08-24 09:23:32 +0530

    video upload

--- models/video.js
+++ models/video.js
@@ -9,7 +9,7 @@ const commentSchema = new mongoose.Schema({
 const videoSchema = new mongoose.Schema({
   title: { type: String, required: true },
   className: { type: String, required: true },
-  subject: { type: String, required: true },
+  subject: { type: String, required: true, set: v => v.toLowerCase() },
   videoUrl: { type: String, required: true },
   uploadedAt: { type: Date, default: Date.now },
 
